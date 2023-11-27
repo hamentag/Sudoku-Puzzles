0adba3a03d7a11395908ed1e5f677c702e6d1ea5
Hamza Amentag <hamentag1@gmail.com>
2023-11-26 23:00:15 -0600

    Added the HTML file and printed the sudoku puzzle on the HTML page

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
+// References
+const sudokuContainer = document.getElementById('sudoku-container');
 
+// State
 const state = {
-    board: null,
+    grid: null,
     puzzle: null,
 }
-function printPuzzle() {
-    for (let i = 0; i < 9; i++) {
-        console.log(state.puzzle[i].join(' '));
-    }
-}
 
-function isValid(board, row, col, num) {
+/**
+ *  Checks if the number is already in the row, column, or 3x3 subgrid
+ * @param {number[][]} grid - a sudoku grid (uncomplete)
+ * @param {number} row - a row index
+ * @param {number} col - a column index
+ * @param {number} num - a number to check
+ * @returns {boolean} a boolean value
+ */
+function isValid(grid, row, col, num) {
     // Check if the number is already in the row or column
-    if (
-        board[row].includes(num) ||
-        board.some(r => r[col] === num)
-    ) {
+    if (grid[row].includes(num) || grid.some(r => r[col] === num)){
         return false;
     }
 
@@ -23,27 +26,31 @@ function isValid(board, row, col, num) {
     const startCol = 3 * Math.floor(col / 3);
     for (let i = 0; i < 3; i++) {
         for (let j = 0; j < 3; j++) {
-            if (board[startRow + i][startCol + j] === num) {
+            if (grid[startRow + i][startCol + j] === num) {
                 return false;
             }
         }
     }
-
     return true;
 }
 
-function solve(board) {
+/**
+ *  solves the sudoku grid
+ * @param {number[][]} grid - a sudoku grid to solve
+ * @returns {boolean} a boolean value: true when the grid is solved
+ */
+function solve(grid) {
     for (let row = 0; row < 9; row++) {
         for (let col = 0; col < 9; col++) {
-            if (board[row][col] === 0) {
+            if (grid[row][col] === 0) {
                 for (let i = 0; i < 9; i++) {
                     const randNum = Math.ceil(Math.random() * 9);
-                    if (isValid(board, row, col, randNum)) {
-                        board[row][col] = randNum;
-                        if (solve(board)) {
+                    if (isValid(grid, row, col, randNum)) {
+                        grid[row][col] = randNum;
+                        if (solve(grid)) {
                             return true;
                         }
-                        board[row][col] = 0;
+                        grid[row][col] = 0;
                     }
                 }
                 return false;
@@ -52,31 +59,59 @@ function solve(board) {
     }
     return true;
 }
-
+/**
+ *  generates a sudoku puzzle
+ */
 function generateSudoku() {
-    // Start with an empty 9x9 board
-    state.board = Array.from({ length: 9 }, () => Array(9).fill(0)); 
+    // Start with an empty 9x9 grid
+    state.grid = Array.from({ length: 9 }, () => Array(9).fill(0)); 
 
-    // Fill the board with a valid solution
-    solve(state.board);
-    console.table(state.board);
+    // Fill the grid with a valid solution
+    solve(state.grid);
+    console.table(state.grid);
 
     // Remove numbers to create the puzzle
-    state.puzzle = [...state.board];
-
+    state.puzzle = [...state.grid];
     for (let i = 0; i < 40; i++) {  
         let row, col;
         do {
             row = Math.floor(Math.random() * 9);
             col = Math.floor(Math.random() * 9);
         } while (state.puzzle[row][col] === 0);
-
         state.puzzle[row][col] = 0;
     }
     console.table(state.puzzle);
 }
 
-// 
+/**
+ *  prints a sudoku puzzle
+ */
+function printPuzzle() {
+    const table = document.createElement('table');
+    for (let i = 0; i < 9; i++) {
+        const row = document.createElement('tr');
+        for (let j = 0; j < 9; j++) {
+            const cell = document.createElement('td');
+            if(state.puzzle[i][j] === 0){
+                const inp = document.createElement('input');
+                inp.type = 'number'
+                inp.id =`input${i}${j}`
+                inp.min = 1
+                inp.max = 9
+                cell.appendChild(inp)
+            }
+            else
+            cell.textContent = state.puzzle[i][j];
+            row.appendChild(cell);
+        }
+        table.appendChild(row);
+    }
+    sudokuContainer.appendChild(table);
+}
+
+/**
+ *  initial state
+ */
 function init(){
     generateSudoku();
     printPuzzle();
